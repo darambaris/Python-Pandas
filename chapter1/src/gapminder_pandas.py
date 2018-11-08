59d47cc83432faced123cd9859ad09350c84f417
Jessika Darambaris <jessika.darambaris@gmail.com>
2018-11-08 01:14:26 -0200

    studying subsets with specific columns and rows

--- chapter1/src/gapminder_pandas.py
+++ chapter1/src/gapminder_pandas.py
@@ -9,6 +9,9 @@ def readCSV():
 	#print first 5 rows
 	print(df.head())
 
+	#print last 5 rows
+	print(df.tail())
+
 	#print type dataFrame
 	print(type(df))
 
@@ -18,13 +21,34 @@ def readCSV():
 	#columns name
 	print(df.columns)
 
-	#uses dtypes to get dtype columns
+	#use dtypes to get dtype columns
 	print(df.dtypes)
 
-	#gets more informations about data
+	#get more informations about data
 	print(df.info())
 
 	return df
 
+def contentSpec(df):
+	#subset with specific columns
+	subset = df[['country', 'continent', 'year']]
+	print (subset.head())
+	print (subset.tail())
+
+	#subset with specific rows
+	print(subset.loc[0])
+	print(subset.loc[subset.shape[0]-1])
+	print(subset.loc[[0, 99, 999]]) #DataFrame
+	#other syntax
+	print (subset.tail(n=1))
+
+	#types diff
+	print (type(subset.tail(n=1))) #DataFrame
+	print (type(subset.head(n=1))) #DataFrame
+	print (type(subset.loc[[0,99,999]])) #DataFrame
+	print (type(subset.loc[subset.shape[0]-1])) #Series
+	
+
 if __name__ == '__main__':
 	df = readCSV()
+	contentSpec(df)
