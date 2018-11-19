14a0a498edf4e7331efcfdd8af4f9ec5122c8764
Jessika Darambaris <jessika.darambaris@gmail.com>
2018-11-19 19:39:04 -0200

    study slicing values and ranges with iloc in df

--- chapter1/src/gapminder_pandas.py
+++ chapter1/src/gapminder_pandas.py
@@ -47,8 +47,41 @@ def contentSpec(df):
 	print (type(subset.head(n=1))) #DataFrame
 	print (type(subset.loc[[0,99,999]])) #DataFrame
 	print (type(subset.loc[subset.shape[0]-1])) #Series
+
+def subSetsColumns(df):
+	#column subset with loc
+	# : means all the rows
+	subset = df.loc[:,['year','pop']]
+	print (subset.head())
+
+	#column subset with iloc
+	subset = df.iloc[:,[2,4,-1]]
+	print(subset.head())
+
+	#convert range to list 
+	list_range = list(range(5))
+	list_interval = list(range(3,6))
+
+	#column subset (0,1,2,3,4) with iloc and range
+	subset_range = df.iloc[:,list_range]
+	subset_interval_range = df.iloc[:,list_interval]
 	
+	print ('Subset range(5):\n {}'.format(subset_range.head()))
+	print ('Subset range(3,6):\n {}'.format(subset_interval_range.head()))
+
+	#column subset with iloc and slicing values
+	subset = df.iloc[:,:5] # = list(range(5))
+	subset_interval = df.iloc[:,3:6]
+
+	print ('Subset slicing values (:5):\n {}'.format(subset.head()))
+	print ('Subset slicing values (3:6): \n {}'.format(subset_interval.head()))
+
+	#slicing and range with step 
+	# range(3,6,2) step 2
+	# slicing [3:6:2] step 2
+
 
 if __name__ == '__main__':
 	df = readCSV()
 	contentSpec(df)
+	subSetsColumns(df)
